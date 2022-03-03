af6c013ad9f53ec3deaffc04be1dcaf95f23ab19
JasnaJ <jovanic_jasna@yahoo.com>
2022-03-03 13:05:22 +0100

    house

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+import {useEffect, useState, useMemo} from 'react';
 import './main-page.css';
 import Header from './header';
 
 function App() {
+  const [allHouses, setAllHouses]= useState([]);
+
+  useEffect(()=>{
+    const fetchHouses = async () =>{
+      const rsp = await fetch("/heoses.json");
+      const houses = rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  },[]);
+
+  const featuredHouse = useMemo(()=>{
+    if(allHouses.lenght){
+      const randomIndex = Math.floor(Math.random()* allHouses.lenght);
+      return allHouses[randomIndex]
+    }
+  },[allHouses]);
   return (
-    <div className='container'>
-      <Header/>
-    </div>
+          <div className='container'>
+            <Header subtitle="Providing houses all over the world"/>
+          </div>
   );
 }
 
